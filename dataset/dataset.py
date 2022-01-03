e3c1383a9a376ec279dcb1fc913df70012762bd5
kirilman <kirilman03@gmail.com>
2022-01-03 22:27:41 +0700

    dataset view

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -5,10 +5,18 @@ from pathlib import Path
 
 
 class AsbestosDataSet:
-    def __init__(self, image_dir, mask_dir) -> None:
+    def __init__(self, image_dir, mask_dir, transform = None) -> None:
         self.image_names = os.listdir(image_dir) 
         self.mask_names = os.listdir(mask_dir)
-        assert len(self.image_names) == len(self.mask_names), 'Number of images and masks are different'
+        self.transform = transform
+        if len(self.image_names) != len(self.mask_names):
+            print('Number of images and masks are different')
+            f_split = lambda x: x.split('.')[0]
+            image_suffix = Path(self.image_names[0]).suffix
+            mask_suffix  = Path(self.mask_names[0]).suffix
+            intersection = list( set(map(f_split, self.image_names)).intersection(list(map(f_split,self.mask_names))))   
+            self.image_names = [s + image_suffix for s in intersection]
+            self.image_paths = [s + mask_suffix for s in intersection]
         self.number_images = len(self.image_names)
         self.image_names.sort(); self.mask_names.sort()
         self.image_paths = [Path(image_dir, name) for name in self.image_names]
@@ -26,19 +34,21 @@ class AsbestosDataSet:
 
     def __getitem__(self, index):
         try:
-            img = Image.open(self.image_paths[index])
-            mask = Image.open(self.mask_paths[index]).convert('L')
+            img = np.array(Image.open(self.image_paths[index]))
+            mask = np.array(Image.open(self.mask_paths[index]).convert('L'))    
             name = self.image_names[index]
+            if self.transform:
+                transformed = self.transform(image=np.array(img), mask=np.array(mask))
+                img = transformed['image']
+                mask= transformed['mask']
             return {'image': img, 'mask': mask, 'path': name}
         except:
             raise 
         
     def __next__(self):
         if self.n < self.number_images:
-            img = Image.open(self.image_paths[self.n])
-            mask = Image.open(self.mask_paths[self.n]).convert('L')
-            name = self.image_names[self.n]
+            res = self.__getitem__(self.n)
             self.n += 1
-            return {'image': img, 'mask': mask, 'path': name}
+            return res
         else:
             raise StopIteration
