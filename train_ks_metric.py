1d99a18598eb2a6e05166738f8618c2d9eb972df
kirilman <kirilman@list.ru>
2022-12-02 18:17:16 +0500

    distance train

--- train_ks_metric.py
+++ train_ks_metric.py
@@ -6,9 +6,10 @@ import numpy as np
 from pathlib import Path
 from dataset.path_utils import get_files_from_dirs, get_paths_from_dirs
 import pandas as pd
-from yolov5.utils.general import xywhn2xyxy, xywh2xyxy, xyxy2xywh
 import scipy.stats as stats
 import subprocess
+import torch
+import numpy
 
 def get_diag(coords):
     if isinstance(coords, np.ndarray) and len(coords.shape) == 2:
@@ -19,6 +20,16 @@ def get_diag(coords):
     else:
         x1,y1,x2,y2 = coords
     return np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
 
 def get_bbox_size_arr(path_2_label, image_names = None):
     labels_files = get_paths_from_dirs([path_2_label],['txt'])
@@ -40,37 +51,35 @@ def ks_metric(a,b):
     return {'statistic': r.statistic, 'pvalue': r.pvalue}
 
 
-
-p = subprocess.Popen(['python', './yolov5/train.py', '--cfg', 'yolov5m.yaml','--data','../dataset/detection_set2/data_simple.yaml', 
-                      '--imgsz', '512', '--name','ks', '--epoch', '5', '--exist-ok'])
+p = subprocess.Popen(['python', './yolov5/train.py', '--weights', 'yolov5m.pt','--data','../dataset/detection_set2/data_simple.yaml', 
+                      '--imgsz', '512', '--name','ks', '--epoch', '3', '--exist-ok', '--batch-size','4'])
 
 p.wait()
 
 log_train = {}
-step = 5
 
-for epoch in range(2,20):
+with open('log_train.txt', 'w') as f:
+    for epoch in range(10):
 
-    p = subprocess.Popen(['python', './yolov5/train.py', '--weights', '../yolov5/runs/train/ks/weights/last.pt',
-                            '--data','../dataset/detection_set2/data_simple.yaml', 
-                            '--imgsz', '512', '--name','ks', '--epoch',str(step), '--exist-ok'])
-    p.wait()
+        p = subprocess.Popen(['python', './yolov5/train.py', '--weights', './yolov5/runs/train/ks/weights/last.pt',
+                                '--data','../dataset/detection_set2/data_simple.yaml', 
+                                '--imgsz', '512', '--name','ks', '--epoch','3', '--exist-ok', '--batch-size','4'])
+        p.wait()
 
-    # !python ../../yolov5/train.py --weights ../../yolov5/runs/train/ks/weights/last.pt --data \
-    # ../../../dataset/detection_set2/data_simple.yaml --imgs 512 --epoch {step} --name ks --batch-size 4 --exist-ok \
-    # --project ../../yolov5/runs/train/
+        p = subprocess.Popen(['python', './yolov5/detect.py', '--weights', './yolov5/runs/train/ks/weights/last.pt',
+                                '--source','../dataset/detection_set2/validation/', 
+                                '--imgsz', '512', '--name','ks','--save-txt', '--exist-ok'])
 
-    p = subprocess.Popen(['python', './yolov5/detect.py', '--weights', '../yolov5/runs/train/ks/weights/last.pt',
-                            '--source','../dataset/detection_set2/validation/', 
-                            '--imgsz', '512', '--name','ks','--save-txt', '--exist-ok'])
+        p.wait()
 
-    p.wait()
+        predict_labels = get_bbox_size_arr('./yolov5/runs/detect/ks/labels/')
+        train_labels   = get_bbox_size_arr('../dataset/detection_set2/validation/')
+        
+        r = ks_metric(predict_labels, train_labels)
+        log_train[epoch] = r
 
-    predict_labels = get_bbox_size_arr('./yolov5/runs/detect/ks/labels/')
-    train_labels   = get_bbox_size_arr('../dataset/detection_set2/validation/')
-    r = ks_metric(predict_labels, train_labels)
-    log_train[epoch*step] = r
-    
+        f.write("{}:{}:{}\n".format(epoch*3, r['statistic'], r['pvalue']))
+        print(epoch)
 frame_log = pd.DataFrame(log_train).T
 frame_log.to_csv('ks_log.csv')
 
